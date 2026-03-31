c306a459624502178429f2d8fe7bdc5efc86cb7d
Crystal Pittman <pittbyte82@gmail.com>
2026-03-31 16:50:34 -0700

    added security headers

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.zoom.us; connect-src 'self' https://*.zoom.us wss://*.zoom.us; frame-src 'self' https://*.zoom.us; media-src 'self' https://*.zoom.us blob:;",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
