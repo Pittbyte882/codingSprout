b7be9d164d1244d6b8ed08d55badcae2ca1d80b0
Crystal Pittman <pittbyte82@gmail.com>
2026-04-15 11:08:10 -0700

    added update

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Toaster } from "sonner"
 import "./styles.css"
 
 export const metadata: Metadata = {
-  title: "Coding Sprout | TK–12 Kids Coding Classes Online ",
-  description: "Coding Sprout teaches TK–12 students how to code through fun, engaging online classes that build real tech skills and confidence.",
+  title: "Coding Sprout | TK–12 Kids Coding Classes Online & In-Person",
+  description: "Coding Sprout teaches TK–12 students how to code through fun, engaging online and in-person classes that build real tech skills and confidence.",
 }
 
 export default function RootLayout({
@@ -15,7 +15,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        
+        <script src="/coi-serviceworker.js" />
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9ZYW4XM703"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9ZYW4XM703');
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
